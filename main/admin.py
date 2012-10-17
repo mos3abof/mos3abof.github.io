84d4eddf3249cedfb2fee155cc81a61217c16505
Mosab Ahmad <mosab.ahmad@gmail.com>
2012-10-18 01:05:51 +0200

    switched to Google App Engine

---
+++ main/admin.py
@@ -0,0 +1,102 @@
+import flask
+from flaskext import wtf
+
+import auth
+import util
+import model
+
+from main import app
+
+
+class ConfigUpdateForm(wtf.Form):
+  brand_name = wtf.TextField(
+      'Brand Name', [wtf.validators.required()]
+    )
+  analytics_id = wtf.TextField(
+      'Analytics ID', [wtf.validators.optional()]
+    )
+  facebook_app_id = wtf.TextField(
+      'Facebook ID', [wtf.validators.optional()]
+    )
+  facebook_app_secret = wtf.TextField(
+      'Facebook Secret', [wtf.validators.optional()]
+    )
+  feedback_email = wtf.TextField('Feedback Email', [
+        wtf.validators.optional(),
+        wtf.validators.email("That doesn't look like an email"),
+      ])
+  twitter_consumer_key = wtf.TextField(
+      'Twitter Key', [wtf.validators.optional()]
+    )
+  twitter_consumer_secret = wtf.TextField(
+      'Twitter Secret', [wtf.validators.optional()]
+    )
+  pubnub_publish = wtf.TextField(
+      'PubNub Publish', [wtf.validators.optional()]
+    )
+  pubnub_subscribe = wtf.TextField(
+      'PubNub Subsrcibe', [wtf.validators.optional()]
+    )
+  pubnub_secret = wtf.TextField(
+      'PubNub Secret', [wtf.validators.optional()]
+    )
+  flask_secret_key = wtf.TextField(
+      'Flask Secret Key', [wtf.validators.required()]
+    )
+
+
+@app.route('/_s/admin/config/', endpoint='admin_config_update_service')
+@app.route(
+    '/admin/config/', methods=['GET', 'POST'], endpoint='admin_config_update',
+  )
+@auth.admin_required
+def admin_config_update():
+  form = ConfigUpdateForm()
+
+  config_db = model.Config.get_master_db()
+  if form.validate_on_submit():
+    config_db.analytics_id = form.analytics_id.data
+    config_db.brand_name = form.brand_name.data
+    config_db.facebook_app_id = form.facebook_app_id.data
+    config_db.facebook_app_secret = form.facebook_app_secret.data
+    config_db.feedback_email = form.feedback_email.data
+    config_db.flask_secret_key = form.flask_secret_key.data
+    config_db.pubnub_publish = form.pubnub_publish.data
+    config_db.pubnub_secret = form.pubnub_secret.data
+    config_db.pubnub_subscribe = form.pubnub_subscribe.data
+    config_db.twitter_consumer_key = form.twitter_consumer_key.data
+    config_db.twitter_consumer_secret = form.twitter_consumer_secret.data
+    config_db.put()
+    update_config_variables(config_db)
+    flask.flash('Your Config settings have been saved', category='success')
+    return flask.redirect(flask.url_for('welcome'))
+  if not form.errors:
+    form.analytics_id.data = config_db.analytics_id
+    form.brand_name.data = config_db.brand_name
+    form.facebook_app_id.data = config_db.facebook_app_id
+    form.facebook_app_secret.data = config_db.facebook_app_secret
+    form.feedback_email.data = config_db.feedback_email
+    form.flask_secret_key.data = config_db.flask_secret_key
+    form.pubnub_publish.data = config_db.pubnub_publish
+    form.pubnub_secret.data = config_db.pubnub_secret
+    form.pubnub_subscribe.data = config_db.pubnub_subscribe
+    form.twitter_consumer_key.data = config_db.twitter_consumer_key
+    form.twitter_consumer_secret.data = config_db.twitter_consumer_secret
+
+  if flask.request.path.startswith('/_s/'):
+    return util.jsonify_model_db(config_db)
+
+  return flask.render_template(
+      'admin/config_update.html',
+      title='Admin Config',
+      html_class='admin-config',
+      form=form,
+      config_db=config_db,
+    )
+
+
+def update_config_variables(config_db):
+  app.config.update(
+      CONFIG_DB=config_db,
+      SECRETE_KEY=config_db.flask_secret_key,
+    )
---
+++ main/main.py
@@ -0,0 +1,220 @@
+import sys
+if not ('lib.zip' in sys.path):
+  sys.path.insert(0, 'lib.zip')
+
+import flask
+from flaskext import wtf
+import config
+
+app = flask.Flask(__name__)
+app.config.from_object(config)
+
+from google.appengine.api import mail
+
+import auth
+import util
+import model
+import admin
+
+
+@app.route('/')
+def welcome():
+  return flask.render_template(
+      'welcome.html',
+      html_class='welcome',
+      channel_name='welcome',
+    )
+
+@app.route('/code')
+def code():
+  return flask.render_template(
+      'code.html',
+      html_class='code',
+      channel_name='code',
+    )
+
+
+################################################################################
+# Profile stuff
+################################################################################
+# class ProfileUpdateForm(wtf.Form):
+#   name = wtf.TextField('Name', [wtf.validators.required()])
+#   email = wtf.TextField('Email', [
+#       wtf.validators.optional(),
+#       wtf.validators.email("That doesn't look like an email"),
+#     ])
+
+
+# @app.route('/_s/profile/', endpoint='profile_service')
+# @app.route('/profile/', methods=['GET', 'POST'], endpoint='profile')
+# @auth.login_required
+# def profile():
+#   form = ProfileUpdateForm()
+#   user_db = auth.current_user_db()
+#   if form.validate_on_submit():
+#     user_db.name = form.name.data
+#     user_db.email = form.email.data.lower()
+#     user_db.put()
+#     return flask.redirect(flask.url_for('welcome'))
+#   if not form.errors:
+#     form.name.data = user_db.name
+#     form.email.data = user_db.email or ''
+
+#   if flask.request.path.startswith('/_s/'):
+#     return util.jsonify_model_db(user_db)
+
+#   return flask.render_template(
+#       'profile.html',
+#       title='Profile',
+#       html_class='profile',
+#       form=form,
+#       user_db=user_db,
+#     )
+
+
+################################################################################
+# Feedback
+################################################################################
+class FeedbackForm(wtf.Form):
+  subject = wtf.TextField('Subject', [wtf.validators.required()])
+  feedback = wtf.TextAreaField('Feedback', [wtf.validators.required()])
+  email = wtf.TextField('Email (optional)', [
+      wtf.validators.optional(),
+      wtf.validators.email("That doesn't look like an email"),
+    ])
+
+
+@app.route('/feedback/', methods=['GET', 'POST'])
+def feedback():
+  form = FeedbackForm()
+  if form.validate_on_submit():
+    mail.send_mail(
+        sender=model.Config.get_master_db().feedback_email,
+        to=model.Config.get_master_db().feedback_email,
+        subject='[%s] %s' % (
+            model.Config.get_master_db().brand_name,
+            form.subject.data,
+          ),
+        reply_to=form.email.data or model.Config.get_master_db().feedback_email,
+        body='%s\n\n%s' % (form.feedback.data, form.email.data)
+      )
+    flask.flash('Thank you for your feedback!', category='success')
+    return flask.redirect(flask.url_for('welcome'))
+  if not form.errors and auth.current_user_id() > 0:
+    form.email.data = auth.current_user_db().email
+
+  return flask.render_template(
+      'feedback.html',
+      title='Feedback',
+      html_class='feedback',
+      form=form,
+    )
+
+
+################################################################################
+# User Stuff
+################################################################################
+# @app.route('/_s/user/', endpoint='user_list_service')
+# @app.route('/user/', endpoint='user_list')
+# def user_list():
+#   user_dbs, more_cursor = util.retrieve_dbs(
+#       model.User,
+#       model.User.query(),
+#       limit=util.param('limit', int),
+#       cursor=util.param('cursor'),
+#       order=util.param('order'),
+#       name=util.param('name'),
+#     )
+
+#   if flask.request.path.startswith('/_s/'):
+#     return util.jsonify_model_dbs(user_dbs, more_cursor)
+
+#   return flask.render_template(
+#       'user_list.html',
+#       html_class='user',
+#       title='User List',
+#       user_dbs=user_dbs,
+#       more_url=util.generate_more_url(more_cursor),
+#     )
+
+
+################################################################################
+# Extras
+################################################################################
+@app.route('/_s/extras/', endpoint='extras_service')
+@app.route('/extras/', endpoint='extras')
+def extras():
+  country = None
+  region = None
+  city = None
+  city_lat_long = None
+  if 'X-AppEngine-Country' in flask.request.headers:
+    country = flask.request.headers['X-AppEngine-Country']
+  if 'X-AppEngine-Region' in flask.request.headers:
+    region = flask.request.headers['X-AppEngine-Region']
+  if 'X-AppEngine-City' in flask.request.headers:
+    city = flask.request.headers['X-AppEngine-City']
+  if 'X-AppEngine-CityLatLong' in flask.request.headers:
+    city_lat_long = flask.request.headers['X-AppEngine-CityLatLong']
+
+  extra_info = {
+    'country': country,
+    'region': region,
+    'city': city,
+    'city_lat_long': city_lat_long,
+    'user_agent': flask.request.headers['User-Agent'],
+  }
+
+  if flask.request.path.startswith('/_s/'):
+    return flask.jsonify(extra_info)
+
+  return flask.render_template(
+      'extras.html',
+      html_class='extras',
+      title='Extras',
+      extra_info=extra_info,
+    )
+
+
+@app.route('/chat/')
+def chat():
+  return flask.render_template(
+      'chat.html',
+      title='Chat',
+      html_class='chat',
+      channel_name='chat',
+    )
+
+
+################################################################################
+# Error Handling
+################################################################################
+@app.errorhandler(400)
+@app.errorhandler(401)
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(410)
+@app.errorhandler(418)
+@app.errorhandler(500)
+def error_handler(e):
+  try:
+    e.code
+  except:
+    class e(object):
+      code = 500
+      name = 'Internal Server Error'
+
+  if flask.request.path.startswith('/_s/'):
+    return flask.jsonify({
+        'status': 'error',
+        'error_code': e.code,
+        'error_name': e.name.lower().replace(' ', '_'),
+        'error_message': e.name,
+      })
+
+  return flask.render_template(
+      'error.html',
+      title='%s!!1' % (e.name),
+      html_class='error-page',
+      error=e,
+    ), e.code
