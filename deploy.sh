8bc039678dd275211e7c38191e5f7570b621173c
Mosab Ahmad <mosab.ahmad@gmail.com>
2016-06-24 03:54:16 +0200

    Updated the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-rsync -avz ./ mosab@mos3abof.com:~/mos3abof.com --exclude ".idea*" --exclude "deploy.sh"
+rsync -avz ./ mosab@mos3abof.com:~/mos3abof.com --exclude ".idea*" --exclude "deploy.sh" --exclude ".git*" --exclude ".gitignore" --delete
 
