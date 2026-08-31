4643f3043528556eaea04ad6a0eaf36b0bd34cdf
mos3abof <mos3abof@users.noreply.github.com>
2026-08-31 01:37:14 +0000

    deploy: 14940710065325205ecbe6d5a8e5a44951db10c2

---
+++ countdown/assets/js/soon/custom.js
@@ -0,0 +1,150 @@
+/******************************************************************************************************************************
+COMMING SOON PAGE
+*******************************************************************************************************************************/
+(function($) {
+  /**
+  * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
+  * according to the GMT+0 Timezone
+  **/
+  var launch = new Date(2024, 2, 15, 0, 0, 0);
+  /**
+  * The script
+  **/
+  var message = $('#message');
+  var years = $('#years');
+  var months = $('#months');
+  var days = $('#days');
+  var hours = $('#hours');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+
+  setDate();
+  function setDate() {
+    var now = new Date();
+    if (launch < now) {
+      years.html('<h1>0</H1><p>Year</p>');
+      months.html('<h1>0</H1><p>Month</p>');
+      days.html('<h1>0</H1><p>Day</p>');
+      hours.html('<h1>0</h1><p>Hour</p>');
+      minutes.html('<h1>0</h1><p>Minute</p>');
+      seconds.html('<h1>0</h1><p>Second</p>');
+      message.html('WE HAVE DONE IT! It is past 5 years in the UK!');
+    }
+    else {
+      var s = -now.getTimezoneOffset() * 60 + (launch.getTime() - now.getTime()) / 1000;
+
+      var y = parseInt(s / 31536000);
+      years.html('<h1>' + y + '</h1><p>Year' + (y > 1 ? 's' : ''), '</p>');
+      s = s - Math.floor(y * 31536000)
+
+      var m = parseInt(s / 2592000);
+      months.html('<h1>' + m + '</h1><p>Month' + (m > 1 ? 's' : ''), '</p>');
+      s = s - Math.floor(m * 2592000)
+
+      var d = Math.floor(s / 86400);
+      days.html('<h1>' + d + '</h1><p>Day' + (d > 1 ? 's' : ''), '</p>');
+
+
+
+      s -= d * 86400;
+
+      var h = Math.floor(s / 3600);
+      hours.html('<h1>' + h + '</h1><p>Hour' + (h > 1 ? 's' : ''), '</p>');
+      s -= h * 3600;
+
+      var m = Math.floor(s / 60);
+      minutes.html('<h1>' + m + '</h1><p>Minute' + (m > 1 ? 's' : ''), '</p>');
+
+      s = Math.floor(s - m * 60);
+      seconds.html('<h1>' + s + '</h1><p>Second' + (s > 1 ? 's' : ''), '</p>');
+      setTimeout(setDate, 1000);
+
+      message.html('Life presents us with yet another countdown!');
+    }
+  }
+})(jQuery);
+/******************************************************************************************************************************
+ANIMATIONS
+*******************************************************************************************************************************/
+(function($) {
+  "use strict";
+  var isMobile = false;
+  if (navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i)) {
+    isMobile = true;
+  }
+  if (isMobile == true) {
+    $('body').removeClass('nomobile');
+    $('.animated').removeClass('animated');
+  }
+  $(function() {
+    if (isMobile == false) {
+      $('*[data-animated]').addClass('animated');
+    }
+    function animated_contents() {
+      $(".animated:appeared").each(function(i) {
+        var $this = $(this),
+          animated = $(this).data('animated');
+
+        setTimeout(function() {
+          $this.addClass(animated);
+        }, 50 * i);
+      });
+    }
+    animated_contents();
+    $(window).scroll(function() {
+      animated_contents();
+    });
+  });
+})(jQuery);
+/******************************************************************************************************************************
+SLIDER
+*******************************************************************************************************************************/
+(function($) {
+  "use strict";
+  $("body.nomobile #slider").revolution(
+    {
+      delay: 9000,
+      startheight: 450,
+      startwidth: 890,
+
+      thumbWidth: 100,
+      thumbHeight: 50,
+      thumbAmount: 5,
+
+      onHoverStop: "on",
+      hideThumbs: 200,
+      navigationType: "bullet",
+      navigationStyle: "round",
+      navigationArrows: "none",
+
+      touchenabled: "on",
+
+      navOffsetHorizontal: 0,
+      navOffsetVertical: 80,
+      shadow: undefined,
+      fullWidth: "on",
+      fullScreen: "on"
+    });
+})(jQuery);
+/******************************************************************************************************************************
+BOOTSTRAP
+*******************************************************************************************************************************/
+(function($) {
+  "use strict";
+  $('[data-rel=tooltip]').tooltip();
+  $(".alert").alert();
+})(jQuery);
+/******************************************************************************************************************************
+PROGRESS BAR
+*******************************************************************************************************************************/
+(function($) {
+  "use strict";
+  $("a.btn-progress").click(function() {
+    $('#bar-container').slideToggle();
+  });
+})(jQuery);
